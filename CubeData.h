e081185479d0e8d52fa3e3b83c5b574fae3d2543
VilsWay <vilas.sadanande@gmail.com>
2020-03-05 23:56:34 +0530

    Add files via upload

--- CubeData.h
+++ CubeData.h
@@ -48,5 +48,46 @@ private:
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
   };
 
+  unsigned int _texturedCubeVAO, _texturedCubeVBO;
+  void _GenerateTextturedCubeVAO();
+
+public : 
+  CubeData()
+  {
+    _GenerateTextturedCubeVAO();
+  }
+  unsigned int& _getTexturedCubeVAO();
+  
 };
+
+unsigned int& CubeData::_getTexturedCubeVAO()
+{
+  return _texturedCubeVAO;
+}
+
+void CubeData::_GenerateTextturedCubeVAO()
+{
+  // 1. Generate
+  glGenVertexArrays(1, &_texturedCubeVAO);
+  glGenBuffers(1, &_texturedCubeVBO);
+  // 2. Bind 
+  glBindVertexArray(_texturedCubeVAO);
+  glBindBuffer(GL_ARRAY_BUFFER, _texturedCubeVBO);
+
+  // 3. Attach Data
+  glBufferData(GL_ARRAY_BUFFER, sizeof(verticesTextured), verticesTextured, GL_STATIC_DRAW);
+
+  // 4. Set VAO index
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0); // 5. Enable Vertex Attrib Array.
+
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1); // 5. Enable Vertex Attrib Array.
+
+  // 6. Unbind
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
+}
+
+
 
